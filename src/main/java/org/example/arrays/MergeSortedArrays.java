8f75bd9a6c842080629c352fa5dcd6e5cf1f456e
Haroon <haroonshinwari@protonmail.com>
2023-06-09 02:02:10 +0100

    Added Arrays: Merge Sorted Arrays with Solution

---
+++ src/main/java/org/example/arrays/MergeSortedArrays.java
@@ -0,0 +1,48 @@
+package org.example.arrays;
+
+import java.util.Arrays;
+
+public class MergeSortedArrays {
+
+    public static void main(String[] args) {
+
+        int[] arr2 = {0,3,4,31,75};
+        int[] arr1 = {4,6,30,55,56,57,85,93,95};
+
+        //mergeSortedArraysBruteForce(arr1, arr2);
+
+        String answer = Arrays.toString(mergeSortedArraysBruteForce(arr1, arr2));
+
+        System.out.println(answer);
+    }
+
+    static int[] mergeSortedArraysBruteForce(int[] array1, int[] array2) {
+
+        int[] emptyArray = new int[array1.length + array2.length];
+
+        int index1 = 0;
+        int index2 = 0;
+
+        for (int i = 0; i < emptyArray.length; i++) {
+
+            if ( ((index2 > array2.length -1 ) && (index1 < array1.length))
+                    && (array1[index1] <= array2[index2]))  {
+                emptyArray[i] = array1[index1];
+                index1++;
+            }else {
+                emptyArray[i] = array2[index2];
+                index2++;
+            }
+        }
+
+        return emptyArray;
+
+
+
+
+
+
+
+
+    }
+}
